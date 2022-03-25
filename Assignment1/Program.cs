a8d62a3adacc1d63766a375f76187f349f70cbd1
Vivek Maniyar <vivekmaniyar9601@gmail.com>
2022-03-25 19:33:48 +0530

    Added Assignment1 of c#.net

---
+++ Assignment1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    public class Program : Igeneric<String>
+    {
+        public String name;
+        public string item { get => name; set => this.item=name; }
+
+        static void Main(string[] args)
+        {
+
+            Program p1 = new Program();
+            Console.WriteLine("This is an example of interface implementation");
+
+            enter:
+            Console.Write("Enter your name: ");
+            p1.name=Console.ReadLine();
+
+            if (p1.name.Length==0)
+            {
+                Console.WriteLine("Please write a name!");
+                goto enter;
+            }
+            else
+            {
+                Console.WriteLine("Welcome, " + p1.printData() + "!");
+            }
+            
+
+            Console.ReadKey();
+        }
+
+        public string printData()
+        {
+            return item;
+        }
+    }
+}
